57e9f974e55d48332324c1b667d51df520307361
Matt Cadorette <mcadorette@gmail.com>
2018-09-25 10:00:26 -0400

    Initial commit from Atomist

---
+++ src/test/java/test1234/Test1234ApplicationTests.java
@@ -0,0 +1,16 @@
+package test1234;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test1234ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
